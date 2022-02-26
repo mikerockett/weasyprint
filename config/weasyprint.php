5101b89504836e0999fb04b4718394262f770e17
Mike Rockett <mrockett@outlook.com>
2022-02-26 13:59:22 +0200

    legacy-chore: add laravel 9 and symfony 6 support; add custom binary path support

--- config/weasyprint.php
+++ config/weasyprint.php
@@ -3,7 +3,7 @@
 return [
   'binary' => env(
     'WEASYPRINT_BINARY',
-    getenv('WEASYPRINT_BINARY') ?? '/usr/local/bin/weasyprint'
+    getenv('WEASYPRINT_BINARY') ?: '/usr/local/bin/weasyprint'
   ),
 
   'cache_prefix' => 'weasyprint-cache_',
